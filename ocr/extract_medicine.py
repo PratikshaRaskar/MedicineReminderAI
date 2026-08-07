d351a2db7c32d0e3af7c52ba4fb8fda66b534fcb
Pratiksha Raskar <pratiksharaskar0@gmail.com>
2026-08-07 20:15:36 +0530

    Update extract_medicine.py

--- ocr/extract_medicine.py
+++ ocr/extract_medicine.py
@@ -1,12 +1,13 @@
 import easyocr
 import cv2
 
-reader = easyocr.Reader(['en'], gpu=False)
-
 def extract_text(image_path):
     """
     Extract all text from a prescription image.
     """
+
+    # Load EasyOCR only when needed
+    reader = easyocr.Reader(['en'], gpu=False)
 
     image = cv2.imread(image_path)
 
