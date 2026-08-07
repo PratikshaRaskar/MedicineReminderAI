2b7bf4698140bd7db9e935540b1a50ed1264f7b5
Pratiksha Raskar <pratiksharaskar0@gmail.com>
2026-08-07 20:12:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,7 +270,5 @@ def success():
 import os
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
