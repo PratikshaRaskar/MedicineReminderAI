5a34730006e5fe5d53c7808ddbc1810cb7ef8524
Pratiksha Raskar <pratiksharaskar0@gmail.com>
2026-08-07 20:03:51 +0530

    Ready for Railway deployment

--- app.py
+++ app.py
@@ -267,9 +267,10 @@ def success():
 # Run Flask Server
 # ==========================================================
 
-if __name__ == "__main__":
+import os
 
+if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
-        port=5000
+        port=int(os.environ.get("PORT", 10000))
     )
